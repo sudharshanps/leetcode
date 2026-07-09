ed8a8e1266032b067e4927e06d98ffead1814004
SUDHARSHAN P S <727724eucy106@skcet.ac.in>
2026-07-09 06:50:52 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (32.34%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int expect = n * (n + 1) / 2;
+
+        int actual = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            actual = actual + nums[i];
+        }
+
+        return expect - actual;
+    }
+}
